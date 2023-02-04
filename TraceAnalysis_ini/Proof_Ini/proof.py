b9a05152d4e408fd2547c2d5ef258b4eeca1d4ba
June-Skeeter <thruhikerflash@gmail.com>
2023-02-03 17:03:06 -0800

    Proof work

--- TraceAnalysis_ini/Proof_Ini/proof.py
+++ TraceAnalysis_ini/Proof_Ini/proof.py
@@ -1,9 +1,12 @@
 import re
+import sys
 from os import walk
 import pandas as pd
 import numpy as np
+
 Year = 2021
 Site = 'BB'
+
 
 Wdir = 'C:\\Users\\User\\PostDoc_Work\\database\\'
 
@@ -22,29 +25,33 @@ for line in Lines:
     count += 1
     if 'inputFileName' in line and '{' in line:
         result = re.search('{\'(.*)\'}', line)
-        Contains.append(result.group(1))
+        Contains.append(result.group(1).split('/')[-1])
 
 Raw_Files = []
-Missing_Files = {}
-Excludes = ['clean','Clean','SecondStage','ThirdStage']
+Missing_Files = {'Name':[],
+                'Path':[]}
+Exclude_keys = ['clean','Clean','SecondStage','ThirdStage','5min']
+Exclude_paths = ['Met\ECCC']
 Path = (Wdir + data_Path)
 proceed = 1
 for (dirpath, dirnames, filenames) in walk(Path):
     rel_dir = dirpath.replace(Path,'')
-    for ex in Excludes:
-        if ex in rel_dir:
+    for ex in Exclude_keys:
+        if ex in rel_dir or rel_dir in Exclude_paths:
             proceed = 0
     if proceed == 1:
 
         if dirpath != Path:
             for file in filenames:
                 if file not in Contains:
-                    Missing_Files[file] = rel_dir
+                    Missing_Files['Path'].append(rel_dir)
+                    Missing_Files['Name'].append(file)
                 else:
                     Raw_Files.append(file)
     proceed = 1
-print(Missing_Files)
+# print(Missing_Files.keys())
 # print(Contains)
-# Missing = pd.DataFrame(index = np.arange(len(Missing_Files)) , data=Missing_Files)
-# print(Missing)
+Missing = pd.DataFrame(data=Missing_Files)
+print(Missing.groupby('Path').count())
+
 
